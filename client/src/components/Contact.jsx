d6771fd9f3ce0f970d5df9139aa8678739fbd3cb
Alina Pisarenko <pisarenko.alina@gmail.com>
2023-03-30 16:05:09 -0400

    added animation for the footer

--- client/src/components/Contact.jsx
+++ client/src/components/Contact.jsx
@@ -1,16 +1,44 @@
-import React from 'react'
+import React, { useState, useRef, useEffect} from 'react'
 import '../styles/Contact.css'
 
 export default function Contact() {
+  const ref = useRef(null);
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    const onScroll = () => {
+        
+        const top = ref.current.offsetTop
+        const windowHeight = window.innerHeight;
+        const currentScrollPos = window.pageYOffset + windowHeight;
+        
+        if (currentScrollPos >= top) {
+          setIsVisible(true);
+        } else {
+          setIsVisible(false);
+        }
+      };
+
+    window.addEventListener('scroll', onScroll);
+    }, []);
+
   return (
     <div className='contact'>
 
       <div className='contact-links'>
-        <a className='contact-link' href="mailto:pisarenko.alina@gmail.com" target="_blank">email</a>
-        <a className='contact-link' href="https://www.linkedin.com/in/alina-pisarenko-web-developer/" target="_blank">linkedin</a>
-        <a className='contact-link' href="https://github.com/AlinaPisarenko" target="_blank">github</a>
+        <div className='contact-link-wrap'>
+            <a className='contact-link' href="mailto:pisarenko.alina@gmail.com" target="_blank">email email</a>
+        </div>
+        
+        <div className='contact-link-wrap'>
+          <a className='contact-link' href="https://www.linkedin.com/in/alina-pisarenko-web-developer/" target="_blank">linkedin linkedin</a>
+        </div>
+        <div className='contact-link-wrap'>
+            <a className='contact-link' href="https://github.com/AlinaPisarenko" target="_blank">github github</a>
+        </div>
       </div>
       <h5 className='contact-title'>Let's work together</h5>
+
     </div>
   )
 }
--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,10 +1,31 @@
-import React from 'react'
+import React, { useRef, useEffect, useState }  from 'react'
 import '../styles/Footer.css'
 
 export default function Footer() {
+
+  const ref = useRef(null);
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    const onScroll = () => {
+        
+        const top = ref.current.offsetTop
+        const windowHeight = window.innerHeight;
+        const currentScrollPos = window.pageYOffset + windowHeight;
+        
+        if (currentScrollPos >= top) {
+          setIsVisible(true);
+        } else {
+          setIsVisible(false);
+        }
+      };
+
+    window.addEventListener('scroll', onScroll);
+    }, []);
+  
   return (
     <div className='footer'>
-      <div className='line' id='line-footer'></div>
+      <div ref={ref}  id='line-footer' className={`line-footer ${isVisible ? "is-visible" : ""}`}></div>
     </div>
   )
 }
