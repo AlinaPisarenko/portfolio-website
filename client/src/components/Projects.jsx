d582ed05dce9db1dc9124d6f1808d05ae8f0a605
Alina Pisarenko <pisarenko.alina@gmail.com>
2023-04-02 23:42:10 -0400

    Revert "fixed bug with project link"
    This reverts commit 1acaf41e0aeceb1b6aa66f854fd732a40a4029b0.

--- client/src/components/Projects.jsx
+++ client/src/components/Projects.jsx
@@ -3,8 +3,13 @@ import { useRef, useEffect, useState } from 'react';
 import '../styles/Projects.css'
 import ProjectCard from './ProjectCard'
 
+const titles = ['Title 1', 'Title 2', 'Title 3'];
+
 export default function Projects({ projects }) {
 
+  const [activeIndex, setActiveIndex] = useState(0);
+  const refArray = useRef([]);
+  // const [titleOffset, setTitleOffset] = useState(0);
 
   const refs = useRef(projects?.map(() => null));
   const [visibleIndices, setVisibleIndices] = useState([]);
@@ -39,10 +44,56 @@ export default function Projects({ projects }) {
     }, []);
   return (
     <div id='projects' className='projects'>
+     {/* {titles.map((title, index) => (
+          <div
+            key={index}
+            className={`content-title ${activeIndex === index ? 'active' : ''}`}
+         
+          >
+            {title}
+          </div>
+        ))} */}
+      {/* {projects.map(project => project.title).map((title, index) => (
+             <div
+             key={index}
+             className={`content-title ${activeIndex === index ? 'active' : ''}`}
+             ref={ref => refArray.current[index] = ref}
+          
+           >
+             {title}
+           </div>
+      ))} */}
+
+{projects.map((project,index) => (
+  <>
+             <div
+             key={index}
+             className={`content-title ${activeIndex === index ? 'active' : ''}`}
+             ref={ref => refArray.current[index] = ref}
+          
+           >
+             {project.title}
+           </div>
+               <a  className='project-link' href={project.link} target='_blank'>open project</a>
+               </>
+      ))}
     
-      {projects.map((project, index) => (
-      <ProjectCard key={project.id} project={project} index={index} refs={refs}  visibleIndices={visibleIndices} />
-    ))}
+    
+      {projects.map((project, index) => {
+
+        return (
+        //   <div>
+        //   <div
+        //   key={index}
+        //   className={`content-title ${activeIndex === index ? 'active' : ''}`}
+       
+        // >
+        //   {project.title}
+        // </div>
+          <ProjectCard key={project.id} project={project} index={index} refs={refs}  visibleIndices={visibleIndices} />
+          // </div>
+        )
+      })}
     </div>
   )
 }
